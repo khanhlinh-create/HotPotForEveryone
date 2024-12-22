1b5893daf2b06c2c13077c18d753e5998d5baff7
Manhx2 <ngothimaianh0401@gmail.com>
2024-12-23 06:18:42 +0700

    Cập nhật gameplay

--- Assets/DragAndDrop.cs
+++ Assets/DragAndDrop.cs
@@ -1,6 +1,8 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 using System.Collections;
+using TMPro;
 
 public class DragAndDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler, IDropHandler
 {
@@ -8,9 +10,11 @@ public class DragAndDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler
 
     private RectTransform rectTransform;
     private CanvasGroup canvasGroup;
-    
-    private void Awake() 
-    { 
+    private Vector2 originalPosition;
+    public TextMeshProUGUI yumText;
+
+    private void Awake()
+    {
         rectTransform = GetComponent<RectTransform>();
         canvasGroup = GetComponent<CanvasGroup>();
     }
@@ -24,6 +28,7 @@ public class DragAndDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler
     public void OnBeginDrag(PointerEventData eventData)
     {
         Debug.Log("OnBeginDrag");
+        originalPosition = GetComponent<RectTransform>().anchoredPosition;
         canvasGroup.alpha = .6f;
         canvasGroup.blocksRaycasts = false;
     }
@@ -34,29 +39,102 @@ public class DragAndDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler
         canvasGroup.alpha = 1f;
         canvasGroup.blocksRaycasts = true;
 
-        //StartCoroutine(DisableDragTemporarily(5f));
+        var hoveredObject = eventData.pointerEnter;
+        if (hoveredObject != null && hoveredObject.GetComponent<ItemsSlot>() != null)
+        {
+            Debug.Log("Dropped into ItemsSlot");
+            StartCoroutine(DisableDragTemporarily(5f));
+        }
+        else
+        {
+            Debug.Log("Not dropped into ItemsSlot");
+            GetComponent<RectTransform>().anchoredPosition = originalPosition;
+        }
+
+        if (hoveredObject != null && hoveredObject.GetComponent<Eat>() != null)
+        {
+            Debug.Log("Dropped into Bowl");
+
+            /*if (yumText != null)
+            {
+                StartCoroutine(DisplayYumText());
+            }*/
+
+            eventData.pointerDrag.SetActive(false);
+        }
+        else
+        {
+            Debug.Log("Not dropped into Bowl");
+        }
     }
 
-    public void OnPointerDown(PointerEventData eventData) 
+    public void OnPointerDown(PointerEventData eventData)
     {
         Debug.Log("OnPointerDown");
     }
 
     public void OnDrop(PointerEventData eventData)
     {
-        
+
     }
 
     private IEnumerator DisableDragTemporarily(float delay)
     {
-        canvasGroup.alpha = 0f;
-        canvasGroup.blocksRaycasts = false;
+        float fadeDuration = 1f;
+        float elapsedTime = 0f;
+
+        var graphic = GetComponent<UnityEngine.UI.Graphic>();
+        if (graphic == null)
+        {
+            Debug.LogError("Graphic component not found!");
+            yield break;
+        }
+
+        Color originalColor = graphic.color;
+        Color targetColor = originalColor * 0f;
+
+        while (elapsedTime < fadeDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            float t = elapsedTime / fadeDuration;
+            graphic.color = Color.Lerp(originalColor, targetColor, t);
+            yield return null;
+        }
+        graphic.color = targetColor;
+
+        if (canvasGroup != null)
+        {
+            canvasGroup.blocksRaycasts = false;
+        }
         Debug.Log("Dragging disabled");
 
         yield return new WaitForSeconds(delay);
 
-        canvasGroup.alpha = 1f;
-        canvasGroup.blocksRaycasts = true;
+        fadeDuration = 1f;
+        elapsedTime = 0f;
+        ColorUtility.TryParseHtmlString("#CDA294", out Color parsedColor);
+        Color resetTargetColor = originalColor * parsedColor;
+
+        while (elapsedTime < fadeDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            float t = elapsedTime / fadeDuration;
+            graphic.color = Color.Lerp(graphic.color, resetTargetColor, t);
+            yield return null;
+        }
+        graphic.color = resetTargetColor;
+
+        if (canvasGroup != null)
+        {
+            canvasGroup.blocksRaycasts = true;
+        }
         Debug.Log("Dragging enabled");
+    }
+
+    private IEnumerator DisplayYumText()
+    {
+        yumText.gameObject.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        yumText.gameObject.SetActive(false);
     }
 }
--- Assets/ItemsSlot.cs
+++ Assets/ItemsSlot.cs
@@ -6,9 +6,5 @@ public class ItemsSlot : MonoBehaviour, IDropHandler
     public void OnDrop(PointerEventData eventData)
     {
         Debug.Log("OnDrop");
-        if (eventData.pointerDrag != null )
-        {
-            //eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
-        }
     }
 }
