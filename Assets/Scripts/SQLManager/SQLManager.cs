0836e20338c584c627fe7dfee7fc4f840d8a419e
pq <67527722+Karin-3@users.noreply.github.com>
2025-01-18 12:48:53 +0700

    .

--- Assets/Scripts/SQLManager/SQLManager.cs
+++ Assets/Scripts/SQLManager/SQLManager.cs
@@ -1,4 +1,4 @@
-/*using UnityEngine;
+﻿using UnityEngine;
 using System.Data.SqlClient;
 
 public class SQLManager : MonoBehaviour
@@ -50,4 +50,4 @@ public class SQLManager : MonoBehaviour
             }
         }
     }
-}*/
+}
